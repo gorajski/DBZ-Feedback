2fc1a6aaf33907e2d9c6442c9f55176e99d9828b
gorajski <nickgorajski@gmail.com>
2016-08-04 00:18:11 -0500

    Get tests to work superficially.  Need to check for understanding.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  let(user1) { User1.create(full_name: "user1", email: "user1@user1") }
-  let(user2) { User2.create(full_name: "user2", email: "user2@user2") }
-  let(feedback) { Feedback.new(recipient_id: 1, show_up?: true, check_in?: true, percent_drive: 40, clarity_of_communication: 4, content: "Four score and seven years ago") }
-  let(review) { Review.new(doable: 4, benevolent: 4, zeroed_in: 4) }
+  let(:user1) { User.create(full_name: "user1", email: "user1@user1") }
+  let(:user2) { User.create(full_name: "user2", email: "user2@user2") }
+  let(:feedback) { Feedback.new(recipient_id: 1, show_up?: true, check_in?: true, percent_drive: 40, clarity_of_communication: 4, content: "Four score and seven years ago") }
+  let(:review) { Review.new(doable: 4, benevolent: 4, zeroed_in: 4) }
 
   before(:each) do
-    review.reviewer = user
+    review.reviewer = user1
     review.feedback = feedback
     review.save
   end
 
   it "describes review's user" do
-    expect(review.reviewer).to eq(user)
+    expect(review.reviewer).to eq(user1)
   end
 
-  it "describes review's user" do
+  it "describes review's feedback" do
     expect(review.feedback).to eq(feedback)
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,28 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(user1) { User1.create(full_name: "user1", email: "user1@user1") }
-  let(user2) { User2.create(full_name: "user2", email: "user2@user2") }
-  let(feedback) { Feedback.new(recipient_id: 1, show_up?: true, check_in?: true, percent_drive: 40, clarity_of_communication: 4, content: "Four score and seven years ago") }
-  let(review) { Review.new(doable: 4, benevolent: 4, zeroed_in: 4) }
+  let(:user1) { User.create(full_name: "user1", email: "user1@user1") }
+  let(:user2) { User.create(full_name: "user2", email: "user2@user2") }
+  let(:feedback) { Feedback.new(show_up?: true, check_in?: true, percent_drive: 40, clarity_of_communication: 4, content: "Four score and seven years ago") }
+  let(:review) { Review.new(doable: 4, benevolent: 4, zeroed_in: 4) }
 
-  it "has a feedback collection" do
-    it "has feedback entries" do
-      expect(user1.feedbacks).to be_empty
-      user1.feedbacks << feedback
-      expect(user1.feedbacks.first).to contain(feedback)
+  describe "associations" do
+    it "user1 has received many pieces of feedback" do
+      expect(user1.received_feedbacks).to be_empty
+      user1.received_feedbacks << feedback
+      expect(user1.received_feedbacks).to include(feedback)
     end
 
-    it "" do
-
+    it "user2 has given many reviews of pieces of feedback" do
+      expect(user2.given_reviews).to be_empty
+      user2.given_reviews << review
+      expect(user2.given_reviews).to include(review)
     end
 
-  end
+    it "user1 has given many pieces of feedback" do
+      expect(user1.given_feedbacks).to be_empty
+      user1.given_feedbacks << feedback
+      expect(user1.given_feedbacks).to include(feedback)
+    end
 
-  it "has a review collection" do
-    expect(user2.review).to be_empty
-    user1.review << review
-    expect(user2.review.first).to contain(review)
+    it "user1 has a reviewed many pieces of feedback" do
+      expect(user1.received_reviews).to be_empty
+      user1.given_feedbacks << feedback
+      # feedback.recipient_id = user2.id
+      feedback.reviews << review
+      expect(user1.received_reviews).to include(review)
+    end
+
+
   end
 
 end

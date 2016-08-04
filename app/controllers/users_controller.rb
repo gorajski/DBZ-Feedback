9371401bc2e6fdcc233a9374f953f12478578669
Joe Duran <joeduran8@gmail.com>
2016-08-04 12:02:56 -0500

    fix rspec tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:full_name, :email, :password, :password_confirmation)
   end
 end

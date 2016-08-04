01d0ed390328a0fa8c8e729571950bf2302dc8d4
Nick Gorajski <email": null>
2016-08-04 17:20:13 -0500

    Adds method of sorting feedback entries in need of reviews.

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -12,4 +12,53 @@ class Feedback < ApplicationRecord
   validates :content, presence: true
   validates_length_of :content, :minimum => 10
 
+  def self.relevant_sample
+
+    relevancy_index = Feedback.build_relevancy_index
+    loop do
+      chosen_id = Feedback.choose_sample(relevancy_index)
+      # if !current_user.is_author?(chosen_id)
+        return Feedback.find(chosen_id)
+      # end
+    end
+
+  end
+
+
+  def self.build_relevancy_index
+    feedbacks = Feedback.all
+
+    unsorted_index = {}      # KEY => index of feedback, VALUE => relevancy_score
+    feedbacks.map do |piece|
+      unsorted_index[piece.id] = piece.age_in_hours - (piece.reviews.count * piece.reviews.count) + piece.author.age_in_hours
+    end
+
+    relevancy_index = unsorted_index.sort_by { |index, score| score }.reverse
+  end
+
+
+  def self.choose_sample(relevancy_index)
+    # relevancy_index[0,100].sample
+    relevancy_index[0][0]
+  end
+
+
+  def age_in_hours
+    ((Time.now - self.created_at)/3600).to_i
+  end
+
+
+  def is_author?(feedback_id)
+    Feedback.find(feedback_id).author == current_user
+  end
+
 end
+
+
+# list = {}
+# fb.map do |piece|
+#   list[piece.id] = ((Time.now - piece.created_at)/3600).to_i - piece.reviews.count + ((Time.now - piece.author.created_at)/3600).to_i
+# end
+
+# [69, 61, 66, 51, 64, 1, 77, 14, 155, 104, 118, 132, 157]
+# 157   69
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,14 @@ class User < ApplicationRecord
   has_many :given_feedbacks, class_name: :Feedback, foreign_key: "author_id"
   has_many :received_feedbacks, class_name: :Feedback, foreign_key: "recipient_id"
   has_many :given_reviews, class_name: :Review, foreign_key: "reviewer_id"
-  has_many :received_reviews, through: :given_feedbacks, source: :reviews 
+  has_many :received_reviews, through: :given_feedbacks, source: :reviews
 
   validates :full_name, presence: true
   validates :email, presence: true
 
-  has_secure_password
+
+  def age_in_hours
+    ((Time.now - self.created_at)/3600).to_i
+  end
 
 end

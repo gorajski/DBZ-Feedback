fa93132ac5c8f5ad1b0f80ce7b756e97a373ed02
William Jung & Nick Gorajski & Laura Rumage <pair+William-Jung.gorajski.laurarumage@devbootcamp.com>
2016-08-05 14:37:57 -0500

    Create view for admins where they can choose best feedbacks, also view bad feedbacks

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,6 +2,7 @@ class AdminsController < ApplicationController
 
   def show
     if logged_in? && current_user.is_admin
+      @hall_of_fames = Feedback.where(check_in: true)
       @user = current_user
     else
       redirect_to '/'
